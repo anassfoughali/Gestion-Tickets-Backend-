9ca398664b8086f381db87670a15007c2aa076ca
Abderrahmane <foutouhabderrahman8@gmail.com>
2026-04-20 09:34:49 +0100

    commit a jmi

--- ticket-service/src/main/java/com/finatech/ticket_service/repository/TicketRepo.java
+++ ticket-service/src/main/java/com/finatech/ticket_service/repository/TicketRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
 @Repository
 public interface TicketRepo extends JpaRepository<Ticket, Long> {
 
